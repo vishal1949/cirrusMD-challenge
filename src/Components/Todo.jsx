7e5e508bc1399bb6b5e8d7e7442a3f309f54cafc
Vishal Sandhu <vishal1949@gmail.com>
2021-07-27 13:34:39 -0700

    Update state depending on checkbox click

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,12 +1,24 @@
-function Todo({todo}){
+import React from 'react'
 
-  return(
-    <div>
-      <div>{todo.id}</div>
-      <div>{todo.title}</div>
-      <div>{todo.completed ? "true": "false"}</div>
-    </div>
-  )
+
+class Todo extends React.Component{
+	constructor(props){
+		super(props)
+		this.state = {
+			todo: this.props.todo
+		}
+	}
+
+	render(){
+		return(
+			<div className="todo">
+				<div>Id: {this.state.todo.id}</div>
+				<div>{this.state.todo.title}</div>
+				<input id={this.state.todo.id} type="checkbox" defaultChecked={this.props.todo.completed} onClick={(e) => this.props.onCheckboxClick(e)}></input> 
+			</div>
+    	)
+  	}
+  
 }
 
 export default Todo
--- src/Components/Todos.jsx
+++ src/Components/Todos.jsx
@@ -12,20 +12,32 @@ class Todos extends React.Component{
 		}
 	}
 
-	componentDidMount(prevState, prevProps){
-		console.log(prevProps)
+	componentDidMount = (prevState, prevProps) => {
 		if(prevProps !== this.props){
-		
 			this.setState({todos: this.props.todos, isLoaded: true})
 		}
 	}
 
+	handleCheckboxClick = ({target}) => {
+		const {checked, id} = target;
+		let results = this.state.todos.map(todo => {
+			if(todo.id == id){
+				todo.completed = checked
+			}
+
+			return todo
+		})
+
+		this.setState({todos: results})
+	}
+
 	render(){
+		console.log(this.state.todos[0]);
 		return (
 			<ul>
 				{this.state.todos.map(todo => (
 					<li key={todo.id} className='list-group-item'>
-						<Todo todo={todo}/>
+						<Todo todo={todo} onCheckboxClick={this.handleCheckboxClick}/>
 					</li>
 				))}	
 			</ul>
